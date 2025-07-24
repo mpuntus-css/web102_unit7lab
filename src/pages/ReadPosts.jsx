5d6e48454ab75ebfcc6d2c2cb919201c87e9f628
Nicholaus <mpuntus@css.edu>
2025-07-23 19:48:15 -0500

    Step 5: Read Database Entries and Step 6: Update Database Entries

--- src/pages/ReadPosts.jsx
+++ src/pages/ReadPosts.jsx
@@ -1,12 +1,22 @@
 import { useState, useEffect } from 'react'
 import Card from '../components/Card'
+import { supabase } from '../client'
+
 
 const ReadPosts = (props) => {
 
     const [posts, setPosts] = useState([])
 
     useEffect(() => {
-        setPosts(props.data)
+        const fetchPosts = async () => {
+            const {data} = await supabase
+            .from('Posts')
+            .select()
+            .order('created_at', { ascending: true })
+
+            setPosts(data)
+        }
+        fetchPosts()
     }, [props])
     
     return (
