ee64d622eef0542cd2cd840e24df03231463fbaa
Nicholaus <mpuntus@css.edu>
2025-07-23 20:11:29 -0500

    Step 6: Update Database Entries and Step 7: Delete Database Entries

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 import { useParams } from 'react-router-dom'
 import './EditPost.css'
+import { supabase } from '../client'
 
 const EditPost = ({data}) => {
 
@@ -17,6 +18,26 @@ const EditPost = ({data}) => {
         })
     }
 
+    const updatePost = async (event) => {
+        event.preventDefault()
+        await supabase
+            .from('Posts')
+            .update({ title: post.title, author: post.author,  description: post.description})
+            .eq('id', id)
+
+         window.location = "/";
+    }
+
+    const deletePost = async (event) => {
+        event.preventDefault()
+
+        await supabase
+            .from('Posts')
+            .delete()
+            .eq('id', id)
+
+        window.location = "/";
+    }
     return (
         <div>
             <form>
@@ -32,8 +53,8 @@ const EditPost = ({data}) => {
                 <textarea rows="5" cols="50" id="description" name="description" value={post.description} onChange={handleChange} >
                 </textarea>
                 <br/>
-                <input type="submit" value="Submit" />
-                <button className="deleteButton">Delete</button>
+                <input type="submit" value="Submit" onClick={updatePost} />
+                <button className="deleteButton" onClick={deletePost}>Delete</button>
             </form>
         </div>
     )
